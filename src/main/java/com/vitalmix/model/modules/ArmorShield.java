ef61229e2e39a513f142b3f4cedaaceffbcf4da2
Vitalij Stinskij <vstinskij@gmail.com>
2021-06-30 14:34:10 +0300

    added ArmorShield class

---
+++ src/main/java/com/vitalmix/model/modules/ArmorShield.java
@@ -0,0 +1,35 @@
+package com.vitalmix.model.modules;
+
+public class ArmorShield extends Module {
+
+    private int damageReduction;
+    private int durability;
+
+    public ArmorShield(String type, String description, int damageReduction, int durability) {
+        super(type, description);
+        this.damageReduction = damageReduction;
+        this.durability = durability;
+    }
+
+    public ArmorShield(int id, String type, String description, int damageReduction, int durability) {
+        super(id, type, description);
+        this.damageReduction = damageReduction;
+        this.durability = durability;
+    }
+
+    public int getDamageReduction() {
+        return damageReduction;
+    }
+
+    public void setDamageReduction(int damageReduction) {
+        this.damageReduction = damageReduction;
+    }
+
+    public int getDurability() {
+        return durability;
+    }
+
+    public void setDurability(int durability) {
+        this.durability = durability;
+    }
+}
