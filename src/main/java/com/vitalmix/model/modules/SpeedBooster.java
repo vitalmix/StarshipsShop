cfd4d5fe2851258b3ea1c0c31c92471ec6638f34
Vitalij Stinskij <vstinskij@gmail.com>
2021-06-30 14:42:28 +0300

    added SpeedBooster

---
+++ src/main/java/com/vitalmix/model/modules/SpeedBooster.java
@@ -0,0 +1,35 @@
+package com.vitalmix.model.modules;
+
+public class SpeedBooster extends Module {
+
+    private int speedBoost;
+    private int timeOfBoost;
+
+    public SpeedBooster(String type, String description, int speedBoost, int timeOfBoost) {
+        super(type, description);
+        this.speedBoost = speedBoost;
+        this.timeOfBoost = timeOfBoost;
+    }
+
+    public SpeedBooster(int id, String type, String description, int speedBoost, int timeOfBoost) {
+        super(id, type, description);
+        this.speedBoost = speedBoost;
+        this.timeOfBoost = timeOfBoost;
+    }
+
+    public int getSpeedBoost() {
+        return speedBoost;
+    }
+
+    public void setSpeedBoost(int speedBoost) {
+        this.speedBoost = speedBoost;
+    }
+
+    public int getTimeOfBoost() {
+        return timeOfBoost;
+    }
+
+    public void setTimeOfBoost(int timeOfBoost) {
+        this.timeOfBoost = timeOfBoost;
+    }
+}
